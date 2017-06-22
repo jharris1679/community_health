e032e523ae4b7d90b290f19d3d9a00fe16eac2ff
Josh Harris <jharris@varagesale.com>
2017-06-21 20:06:16 -0400

    looking correlations between network stats, added total transactions to dataset

--- sql/transaction_network_properties.sql
+++ sql/transaction_network_properties.sql
@@ -2,12 +2,13 @@ select
   'transaction' as edge_type,
   p.cid,
   name,
-  nodes, 
   nodes*(nodes-1)/2 as possible_edges, 
   actual_edges,
-  if(nodes*(nodes-1)/2=0, 0, actual_edges/(nodes*(nodes-1)/2))*100 as network_denstiy_X_100,
+  nodes, 
+  if(nodes*(nodes-1)/2=0, 0, actual_edges/(nodes*(nodes-1)/2))*100 as network_density_X_100,
   2*actual_edges/nodes as avg_degree,
-  avg_weight
+  avg_weight,
+  round(nodes*(2*actual_edges/nodes)*avg_weight, 0) total_transactions
 from
   (select m_community_id cid, name, count(distinct(memb_id)) nodes
   from `vs_reporting.user_memberships` u
@@ -22,15 +23,15 @@ join
     where buyer_id != 'NULL') t on u.user_id = t.buyer_id
   group by cid) a on p.cid = a.cid
 join 
-  (select cid, avg(weight) avg_weight
+  (select cid, avg(edge_weight) avg_weight
   from
-    (select m_community_id cid, 2^buyer_id*2^seller_id actual_edges, count(distinct(item_id)) weight
+    (select m_community_id cid, 2^buyer_id*2^seller_id edge, count(distinct(item_id)) edge_weight
     from vs_reporting.user_memberships u
     join 
       (select cast(buyer_id as int64) as buyer_id, seller_id, item_id
       from `vs_reporting.transactions`
       where buyer_id != 'NULL') t on u.user_id = t.buyer_id
-    group by cid, actual_edges)
+    group by cid, edge)
   group by cid) w on w.cid = p.cid
 where nodes > 100
 order by cid
